c892a97f48d4789d5afc1c892dbb50a060a5a762
informatik409 <al1movdev.2008@gmail.com>
2023-01-13 20:10:35 +0500

    first commit

--- src/components/app/router/ContactPage.vue
+++ src/components/app/router/ContactPage.vue
@@ -6,6 +6,12 @@
         <div class="container">
             <form @submit.prevent="onSend" >
             <div class="mb-3">
+                <div class="alert">
+                    <p id="p">Message has been sent!! 😊</p>
+                    <p id="close">❌</p>
+                </div>
+            </div>
+            <div class="mb-3">
                 <label for="full_name" class="form-label">Full Name</label>
                 <input required v-model="fullname" type="text" class="form-control" id="name">
             </div>
@@ -32,7 +38,6 @@ export default {
             email: "",
             message: "",
 
-
             //Token Telegram BOT 
             token: "5781325179:AAHBFeKy4am0UNN7Wfu3Z50b6g9MtCKpfdU",
             chatID: 5027238978
@@ -41,13 +46,26 @@ export default {
     
     methods: {
         onSend() {
+            const closeBtn = document.querySelector("#close");
+            const alert = document.querySelector(".alert");
+            const name = document.querySelector("#name");
+            const email = document.querySelector("#email");
+            const message = document.querySelector("#message");
             const fullMessage = `Full Name: ${this.fullname}  \nEmail: ${this.email}  \nMessage: ${this.message}`
             axios.post(`https://api.telegram.org/bot${this.token}/sendMessage?chat_id=${this.chatID}&text=${fullMessage}`)
             .then( response => {
-                console.log("Successfully");
+                alert.style.display = 'block'
+                name.value = null;
+                email.value = null;
+                message.value = null;
             }, error => {
                 console.log(error)
+            });
+
+            closeBtn.addEventListener("click", ()=> {
+                alert.style.display = 'none'
             })
+
         }
     },
 }
@@ -81,6 +99,60 @@ export default {
 
     .container button {
         border: 1px solid grey;
+    }
+
+    .alert {
+        top: 90px;
+        right: 10px; 
+        position: absolute;
+        background-color: #fff;
+        height: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 300px;
+        display: none;
+        animation-name: animate;
+        animation-duration: 0.2s;
+    }
+
+    @keyframes animate {
+        0% {
+            right: 0px;
+            transform: scale(0.0);
+        }
+        25% {
+            right: 3;
+            transform: scale(0.3);
+        }
+        50% {
+            right: 5px;
+            transform: scale(0.6);
+        }
+        75% {
+            right: 7;
+            transform: scale(0.8);
+        }
+        100% {
+            right: 10px;
+            transform: scale(0.9);
+        }
+    }
+
+    .alert #p {
+        color: black;
+        font-family: "Roboto", sans-serif;
+        font: 15px Rubik, san-serif;
+        text-align: start;
+        margin: 0;
+        padding: 1px;
+    }
+
+    .alert #close {
+        cursor: pointer;
+        position: absolute;
+        top: 3px;
+        right: 5px;
     }
 
 </style>
