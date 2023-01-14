052298a5860645f2b0d7d1cf292ad561a2638663
informatik409 <al1movdev.2008@gmail.com>
2023-01-14 10:03:53 +0500

    second commit

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -11,7 +11,7 @@
         <div class="nav-img">
             <!-- <a href="#"> -->
               <router-link to="/">
-                <img src="@/assets/img/logo.png" alt="">
+                <img src="@/assets/img/logo.png" alt="logo">
               </router-link>
             <!-- </a> -->
         </div>
@@ -85,6 +85,7 @@
                 <!-- </a> -->
               </router-link>
             </div>
+            <div><img src="https://mc.yandex.ru/watch/92053612" style="position:absolute; left: 10px; top: 0;" alt="" /></div>
         </div>
       </nav>
     <!-- End Footer Navbar -->
@@ -92,10 +93,18 @@
     <router-view></router-view>
 </template>
 <script>
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-export default {
-
-}
+   ym(92053612, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
 </script>
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
